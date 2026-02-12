961d7c551030aa20842bc30b57180d3319111cdf
ak8868-blip <ak8868@srmist.edu.in>
2026-02-12 10:41:30 +0530

    added pop.java

---
+++ src/pop.java
@@ -0,0 +1,14 @@
+public class pop {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+
+        int sum = a + b + c + d + e;
+
+        System.out.println("Sum = " + sum);
+    }
+}
